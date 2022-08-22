bca0f743782e99d26bd728f7ada7f7c480459950
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-22 04:42:06 -0700

    added lecture memo about stacks and queues

---
+++ Stack-Queue/stack-queue.js
@@ -0,0 +1,30 @@
+/*
+
+Stacks
+- LIFO: you can think of them as plates piling up -> you can only touch the last plate
+- lookup -> O(n)
+- pop -> O(1)
+- push -> O(1)
+- peek -> O(1)
+
+Queues
+- FIFO: you can think of them as people lining up for a roller coaster
+- lookup -> O(n)
+- enqueue -> O(1)
+- dequeue -> O(1)
+- peek -> O(1)
+- you don't want to use an array
+
+*/
+
+// << Stacks >>
+// ex. browser history
+// google -> udemy -> facebook -> twitter
+// you can build stacks with arrays or linked lists pretty well
+
+// << Queues >>
+// ex. waiting list
+// Mike <- Joy <- Emma <- Kate
+// Head                   Tail pointer
+// you can build stacks with arrays or linked lists but using linked lists is a good idea
+

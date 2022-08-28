8dfe95394f991cfe37c7ebf6ba329c5cda8af080
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-28 04:45:44 -0700

    added data structure - tree

---
+++ BigO/example-o(logn).js
@@ -0,0 +1,18 @@
+// Binary Tree
+
+/*
+
+Lv0: 2^0 -> 1
+Lv1: 2^1 -> 2
+Lv2: 2^2 -> 4
+Lv3: 2^3 -> 8
+Lv4: 2^4 -> 16
+
+h: height
+The number of nodes = 2^h - 1;
+-> log nodes = steps(to reach out to the specific node)
+
+*/
+
+// log(10) 100 = 10
+// log(2) 8 = 3
---
+++ Tree/binary-heap.js
@@ -0,0 +1,19 @@
+//      101
+//   72     33
+// 2   45  5   1
+
+// lookup -> O(n)
+// insert -> O(log N)
+// delete -> O(log N)
+
+/*
+pros
+- better than O(n)
+- priority
+- flexible size
+- fast insert
+
+cons
+- slow lookup
+
+*/
